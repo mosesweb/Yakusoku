7427905dccb79248b5e7d22cf89874324e8488a9
Moses J <mosesjohansson@gmail.com>
2022-04-25 16:29:12 +0200

    add auth listener

--- app/src/main/java/com/fileidea/myapplication/MainActivity.kt
+++ app/src/main/java/com/fileidea/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -13,28 +14,38 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     private val mAuth = FirebaseAuth.getInstance();
+    private lateinit var mFirebaseAuthLis: FirebaseAuth.AuthStateListener
 
     override fun onStart() {
+        try {
+            mAuth.addAuthStateListener(mFirebaseAuthLis)
+        }
+        catch(e: Exception) {
+            Log.e("Info","Error! ${e.message}")
+        }
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser: FirebaseUser? = mAuth.currentUser
-        updateUI(currentUser)
     }
+
+
 
     fun updateUI(user: FirebaseUser?) {
         val hasUser = user != null
         Log.i("updateUI", "IS USER NOT NULL?.." )
+        Log.i("updateUI",  user?.email.toString())
+
         if(hasUser)
         {
            // signout_button.isVisible = true
             signup_button.isVisible = false
-            Log.i("updateUI",  user?.email.toString())
         }
         else {
           //  signup_button.isVisible = true
@@ -60,17 +71,35 @@ class MainActivity : AppCompatActivity() {
                 )
             }
             .addOnFailureListener { e -> Log.w("Info", "Error adding document", e) }
+
+
+        mFirebaseAuthLis = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            var user = firebaseAuth.currentUser
+            if (user != null) {
+                Log.i("updateUI", "hej." )
+                Toast.makeText(applicationContext,"You are logged in as ${user.email}",Toast.LENGTH_SHORT).show()
+            } else {
+                Log.i("updateUI", "hej2" )
+                updateUI(user)
+                // Logged out
+                Toast.makeText(applicationContext,"You are not logged in",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
 
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun SignUpButtonEvent(view: android.view.View) {
             val intent = Intent(this, RegisterForm::class.java).apply {
             }
             startActivity(intent)
+    }
+    fun SignOutButtonEvent(view: android.view.View) {
+        Log.i("Info", "logout..")
+        mAuth.signOut();
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
